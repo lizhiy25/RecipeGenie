eb991743edfb34bce52b64e5240a30f06b7e7763
Brandon <bcloud.yang@mail.utoronto.ca>
2024-11-29 17:16:21 -0500

    Update with Refractor for the class SavedFile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,16 +20,20 @@ public class Main {
 class Saved {
     public static void main(String[] args) {
         // Create an instance of SaveFile
-        SaveFile saveFile = new SaveFile();
+        SaveFile saveFile = SaveFile.getInstance(); // Use the singleton instance
 
-        // Add a recipe using the Nutrition API
+        // Add a recipe using the Nutrition API (name and nutritional information)
         saveFile.fetchAndAddRecipe("1lb brisket and fries");
 
         // Print all saved recipes
         System.out.println("Saved Recipes:");
-        for (String url : saveFile.getRecipeUrl()) {
-            System.out.println(url);
+        List<String[]> savedRecipes = saveFile.getSavedRecipes(); // Get both name and nutritional info
+        for (String[] recipe : savedRecipes) {
+            String recipeName = recipe[0];
+            String nutritionalInfo = recipe[1];
+            System.out.println("Recipe: " + recipeName);
+            System.out.println("Nutritional Info: " + nutritionalInfo);
         }
-
     }
 }
+
